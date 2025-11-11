f1b55e721fa0f00025cba32095084d3d82c5a07e
Kpoji4er <kpojiomen@gmail.com>
2025-11-11 19:09:25 +0300

    fix

--- Code/CombatAI.lua
+++ Code/CombatAI.lua
@@ -961,10 +961,10 @@ function AIBuildArchetypePaths(unit, pos, context)
 	local goto_stance = archetype.MoveStance
 	local pref_stance = archetype.PrefStance
 
-	if context.restarts and context.restarts > 0 then
-		goto_stance = unit.stance
-		pref_stance = unit.stance
-	end
+--	if context.restarts and context.restarts > 0 then
+--		goto_stance = unit.stance
+--		pref_stance = unit.stance
+--	end
 
 	--[[local current_stance = unit.stance or pref_stance
 
